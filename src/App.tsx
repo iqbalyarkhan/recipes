ee8bcd0bdc262f194c14540f72bd5f07808bea97
iqbalyarkhan <iqbal.khan@utdallas.edu>
2023-03-04 18:24:40 -0600

    add recipe skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { Recipe } from "../src/Recipe";
+import { Ingredients } from "./Ingredients";
+import { Instructions } from "./Instructions";
+import { Title } from "./Title";
 
 function App() {
   return (
     <div className="App">
       <h1>Main App Component</h1>
+      <Title />
       <Recipe />
+      <Ingredients />
+      <Instructions />
     </div>
   );
 }
